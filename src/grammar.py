1d806e18445fec640db814293a03e3efe0c5a478
Chris Dent <chris.dent@gmail.com>
2011-09-06 15:12:55 +0100

    First pass grammar.py for creating python grammar from yaml grammar.
    made with the magic of cut and paste!

---
+++ src/grammar.py
@@ -0,0 +1,29 @@
+
+import sys
+import yaml
+import pprint
+
+def run():
+    try:
+        grammar = yaml.load(open(sys.argv[1]))
+    except:
+        raise # XXX
+
+    data = pprint.pformat(grammar, indent=2)
+
+    module = """\
+\"\"\"
+Pegex Grammar module for pegex
+\"\"\"
+
+class Grammar():
+    def __init__(self):
+        self.__dict__.update(
+        %(data)s
+        )
+""" % locals()
+
+    print module
+
+if __name__ == '__main__':
+    run()
